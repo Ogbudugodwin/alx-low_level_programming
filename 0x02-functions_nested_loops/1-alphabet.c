d5c38f32d28a4415d6f9399d9f1429431c471851
ogbudugodwin <ogbudugodwin6@gmail.com>
2023-10-02 05:39:39 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the main function
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
